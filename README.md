504106d2ec9e79bcc2432230bc0782fefabc6446
Aleksandar Petrovski <8148660+alpetrovski@users.noreply.github.com>
2021-12-04 09:11:41 +0100

    Day 03.

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 |-----|---------------------------------------------------------------|
 | 01  | [Part1](src/day01/part01.kt) and [Part2](src/day01/part02.kt) |
 | 02  | [Part1](src/day02/part01.kt) and [Part2](src/day02/part02.kt) |
+| 03  | [Part1](src/day03/part01.kt) and [Part2](src/day03/part02.kt) |
---
+++ src/day03/part01.kt
@@ -0,0 +1,34 @@
+package day03
+
+import java.nio.file.Paths
+
+fun main() {
+    val input = Paths.get("src/day03/input.in").toFile().readLines()
+    val ones = arrayOfNulls<Int>(input[0].length)
+
+    input.forEach {
+        (ones.indices).forEach { index ->
+            if (it[index] == '1') {
+                ones[index] = (ones[index] ?: 0) + 1
+            }
+        }
+    }
+
+    val gamma = Integer.parseInt(ones.map {
+        if ((it ?: 0) > (input.size / 2)) {
+            '1'
+        } else {
+            '0'
+        }
+    }.joinToString(""), 2)
+
+    val epsilon = Integer.parseInt(ones.map {
+        if ((it ?: 0) > (input.size / 2)) {
+            '0'
+        } else {
+            '1'
+        }
+    }.joinToString(""), 2)
+
+    println("RESULT: ${gamma * epsilon}")
+}
