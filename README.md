811d3b622cbec250daa5e0ade8257fba9e1b7ebf
Aleksandar Petrovski <8148660+alpetrovski@users.noreply.github.com>
2021-12-04 08:43:17 +0100

    Day 02.

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 | Day | Solution                                                      |
 |-----|---------------------------------------------------------------|
 | 01  | [Part1](src/day01/part01.kt) and [Part2](src/day01/part02.kt) |
+| 02  | [Part1](src/day02/part01.kt) and [Part2](src/day02/part02.kt) |
---
+++ src/day02/part01.kt
@@ -0,0 +1,17 @@
+package day02
+
+import java.nio.file.Paths
+
+fun main() {
+    val aggregatedCommands = Paths.get("src/day02/input.in").toFile().readLines().map {
+        val lineComponents = it.split(" ")
+        Pair(lineComponents[0], lineComponents[1].toLong())
+    }.groupBy { it.first }
+        .map { entry -> entry.key to entry.value.sumOf { it.second } }
+        .toMap()
+
+    val result = (aggregatedCommands["down"]!! - aggregatedCommands["up"]!!) *
+            aggregatedCommands["forward"]!!
+
+    println("RESULT: $result")
+}
