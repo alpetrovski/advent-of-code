4b9c0d71a70a226afe144cf386debd5b59dd112a
Aleksandar Petrovski <8148660+alpetrovski@users.noreply.github.com>
2022-11-30 22:40:17 +0100

    Create module for 2022.

---
+++ 2022-kotlin/src/com/aleksandarpetrovski/aoc2022/day01/part01.kt
@@ -0,0 +1,7 @@
+package com.aleksandarpetrovski.aoc2022.day01
+
+import java.nio.file.Paths
+
+fun main() {
+    Paths.get("2022-kotlin/src/day01/input.in").toFile().readLines()
+}
---
+++ 2022-kotlin/src/com/aleksandarpetrovski/aoc2022/playground/playground.kt
@@ -0,0 +1,5 @@
+package com.aleksandarpetrovski.aoc2022.playground
+
+fun main() {
+    println("Hello world!")
+}
