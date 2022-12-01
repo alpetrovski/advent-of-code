a3e76176ab83c6464a1d5f60d4f41acd1086fa70
Aleksandar Petrovski <8148660+alpetrovski@users.noreply.github.com>
2022-12-01 06:42:41 +0100

    Day 1 2022.

--- 2022-kotlin/src/com/aleksandarpetrovski/aoc2022/day01/part0102.kt
+++ 2022-kotlin/src/com/aleksandarpetrovski/aoc2022/day01/part0102.kt
@@ -2,18 +2,18 @@ package com.aleksandarpetrovski.aoc2022.day01
 
 import java.nio.file.Paths
 
+private const val INPUT_FILE = "2022-kotlin/src/com/aleksandarpetrovski/aoc2022/day01/input.in"
+
 fun main() {
-    val sortedElfCalories = Paths.get("2022-kotlin/src/com/aleksandarpetrovski/aoc2022/day01/input.in").toFile().readLines()
-        .joinToString(",")
-        .split(",,").map {elfCalories ->
+    val sortedElfCalories = Paths.get(INPUT_FILE).toFile().readLines()
+        .joinToString(",").split(",,")
+        .map {elfCalories ->
             elfCalories.split(",").sumOf { it.toInt() }
         }.sortedDescending()
 
     val result1 = sortedElfCalories.first()
-    println("EXPECTED RESULT 1: 65912")
     println("RESULT 1: $result1")
 
     val result2 = sortedElfCalories.subList(0, 3).sum()
-    println("EXPECTED RESULT 2: 195625")
     println("RESULT 2: $result2")
 }
