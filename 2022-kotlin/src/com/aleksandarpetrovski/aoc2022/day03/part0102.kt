30e92d4157dd8255657a36e09d42c7773811811c
Aleksandar Petrovski <8148660+alpetrovski@users.noreply.github.com>
2022-12-03 07:05:04 +0100

    2022 Day 03 solution.

--- 2022-kotlin/src/com/aleksandarpetrovski/aoc2022/day03/part0102.kt
+++ 2022-kotlin/src/com/aleksandarpetrovski/aoc2022/day03/part0102.kt
@@ -5,9 +5,8 @@ import java.nio.file.Paths
 
 private const val INPUT_FILE = "2022-kotlin/src/com/aleksandarpetrovski/aoc2022/day03/input.in"
 
-private fun findFirstCommonChar(inputs: List<CharArray>) = inputs.first().first { itemToBeFound ->
-    inputs.subList(1, inputs.size).all { it.contains(itemToBeFound) }
-}
+private fun findFirstCommonChar(inputs: List<String>) =
+    inputs.map { it.toSet() }.reduce { acc, chars -> acc.intersect(chars) }.first()
 
 private fun itemPriority(input: Char) = when (input) {
     in 'a'..'z' -> input - 'a' + 1
@@ -18,10 +17,10 @@ private fun itemPriority(input: Char) = when (input) {
 fun main() {
     val inputLines = Paths.get(INPUT_FILE).toFile().readLines()
     val result1 = inputLines.sumOf { rucksack ->
-        val itemsInRucksackCount = rucksack.length
-        val compartmentA = rucksack.substring(0, (itemsInRucksackCount / 2))
-        val compartmentB = rucksack.substring((itemsInRucksackCount / 2))
-        val commonItem = findFirstCommonChar(listOf(compartmentA.toCharArray(), compartmentB.toCharArray()))
+        val middleOfItemsStream = rucksack.length / 2
+        val compartmentA = rucksack.substring(0, middleOfItemsStream)
+        val compartmentB = rucksack.substring(middleOfItemsStream)
+        val commonItem = findFirstCommonChar(listOf(compartmentA, compartmentB))
         itemPriority(commonItem)
     }
 
@@ -29,13 +28,11 @@ fun main() {
         .groupBy { it.index / 3 }
         .map { it.value }
         .sumOf { elfGroup ->
-            itemPriority(findFirstCommonChar(elfGroup.map { it.value.toCharArray() }))
+            val commonItem = findFirstCommonChar(elfGroup.map { it.value })
+            itemPriority(commonItem)
         }
 
-    println("Expected Result 1: 7990")
     println("Result 1: $result1")
-
-    println("Expected Result 2: 2602")
     println("Result 2: $result2")
 }
 
