e5e0d45aea4717b5300de374138899f3a052e23d
Aleksandar Petrovski <8148660+alpetrovski@users.noreply.github.com>
2022-12-06 08:46:21 +0100

    Day 06 improvement.

--- aoc-2022-kotlin/src/com/aleksandarpetrovski/aoc2022/day06/part0102.kt
+++ aoc-2022-kotlin/src/com/aleksandarpetrovski/aoc2022/day06/part0102.kt
@@ -4,10 +4,10 @@ import com.aleksandarpetrovski.aoc2022.readInputLines
 
 fun main() {
     val inputStream = readInputLines(day = 6)[0]
-    println("Result Part 1: ${inputStream.firstNDistinctChars(4)}")
-    println("Result Part 2: ${inputStream.firstNDistinctChars(14)}")
+    println("Result Part 1: ${inputStream.firstDiverseWindow(4)}")
+    println("Result Part 2: ${inputStream.firstDiverseWindow(14)}")
 }
 
-private fun String.firstNDistinctChars(differentChars: Int) = (differentChars until this.length).first { windowEnd ->
-    (windowEnd - differentChars until windowEnd).distinctBy { this[it] }.count() == differentChars
+private fun String.firstDiverseWindow(windowSize: Int) = (windowSize until this.length).first { windowEnd ->
+    (windowEnd - windowSize until windowEnd).distinctBy { this[it] }.count() == windowSize
 }
