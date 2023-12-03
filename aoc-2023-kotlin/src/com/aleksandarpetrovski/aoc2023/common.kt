1bec3208753a15fa5764a4cd112b653d7f26bb7b
Aleksandar Petrovski <8148660+alpetrovski@users.noreply.github.com>
2023-12-03 09:44:26 +0100

    [2023] Init structure

---
+++ aoc-2023-kotlin/src/com/aleksandarpetrovski/aoc2023/common.kt
@@ -0,0 +1,10 @@
+package com.aleksandarpetrovski.aoc2023
+
+import java.nio.file.Paths
+
+fun readInputLines(day: Int, isTest: Boolean = false): List<String> {
+    val paddedDay = day.toString().padStart(2, '0')
+    val fileName = if (isTest) "test-input.in" else "input.in"
+    return Paths.get("aoc-2022-kotlin/src/com/aleksandarpetrovski/aoc2023/day${paddedDay}/${fileName}")
+        .toFile().readLines()
+}
---
+++ aoc-2023-kotlin/src/com/aleksandarpetrovski/aoc2023/playground/playground.kt
@@ -0,0 +1,5 @@
+package com.aleksandarpetrovski.aoc2023.playground
+
+fun main() {
+    println("Hello world!")
+}
